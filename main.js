a5a0a6d12af83c7f3cb6f4f26ca08903cd20b8bc
ValentinaNovarino <valenovarino@gmail.com>
2020-11-20 15:03:57 +0100

    log email casuale

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ var app = new Vue (
     {
         el: '#root',
         data: {
-
+            emailCasual: false
         },
         mounted() {
+            const self = this;
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(function(risposta) {
-                console.log(risposta);
+                self.emailCasual = risposta.data.response;
+                console.log(self.emailCasual);
             })
         }
 
